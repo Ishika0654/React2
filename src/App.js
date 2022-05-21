08b11ecfc6fcb13c5c0b730af451b78bd7abb794
Ishika Singla <102655316+Ishika0654@users.noreply.github.com>
2022-05-21 14:59:30 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+
+import "./styles.css";
+
+export default function App() {
+  const[num1,setNum1] = useState();
+  const[num2,setNum2] = useState();
+  const[answer,setAnswer] = useState();
+
+  const changeNum1 = (event)=> {
+    setNum1(event.target.value);
+  };
+  const changeNum2 = (event)=> {
+    setNum2(event.target.value);
+  };
+
+  const handleClick = () => {
+    var sum = Number(num1) + Number(num2);
+    setAnswer(sum);
+  };
+  
+  return (
+    <>
+    <input onChange={changeNum1}value={num1}/>
+    <input onChange={changeNum2}value={num2}/>
+    <button onClick={handleClick}>ADD</button>
+    <div>{answer}</div>
+    </>
+  );
+}
+
